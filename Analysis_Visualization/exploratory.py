a1f00c2221328702bb149e641d53fca9dab44e43
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-02-06 19:45:10 +0900

    data analyze

--- Analysis_Visualization/exploratory.py
+++ Analysis_Visualization/exploratory.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-reporty = pd.read_csv('../Data/neumo_dataset.tsv', sep='\t', header=0)
+cols = ["ImageID", "StudyDate_DICOM", "StudyID", "PatientID",
+        "PatientBirth", "PatientSex_DICOM", "Pediatric",
+        "Labels", "group", "Partition", "Subject_occurrences",
+        "Partition_occurrences", "Partitionlabel_occurrences"]
 
-print(reporty.head())
+reporty = pd.read_csv('../Data/neumo_dataset_balanced_0.tsv', usecols=cols, sep='\t')
+
+print(reporty.to_string())
