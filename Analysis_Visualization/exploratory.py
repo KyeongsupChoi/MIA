fb978b32f8d050fd1dfa467d896da6385be35f33
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-02-09 10:03:58 +0900

    processed csv

--- Analysis_Visualization/exploratory.py
+++ Analysis_Visualization/exploratory.py
@@ -7,4 +7,21 @@ cols = ["ImageID", "StudyDate_DICOM", "StudyID", "PatientID",
 
 reporty = pd.read_csv('../Data/neumo_dataset_balanced_0.tsv', usecols=cols, sep='\t')
 
-print(reporty.to_string())
+df_exploded = reporty.explode('Labels')
+
+# Count the occurrences of each value
+value_counts = df_exploded['Labels'].value_counts()
+
+print(value_counts[0:10])
+
+
+# ['normal']                            9004
+# ['pneumonia']                          783
+# ['infiltrates']                        739
+# ['infiltrates', ' pneumonia']          478
+# ['consolidation', ' pneumonia']        210
+# ['consolidation']                      4
+
+sliced = reporty[reporty['Labels'].isin(["['normal']", "['consolidation', ' pneumonia']"])]
+print(len(sliced))
+sliced.to_csv("sliced.csv", encoding='utf-8', index=False)
