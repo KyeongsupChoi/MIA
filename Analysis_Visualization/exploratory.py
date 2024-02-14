6d95c0ff4c64d3d8c80c169c91cfd5b8bba396fa
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-02-14 09:16:52 +0900

    comment exploratory

--- Analysis_Visualization/exploratory.py
+++ Analysis_Visualization/exploratory.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
+# Define columns of interest
 cols = ["ImageID", "StudyDate_DICOM", "StudyID", "PatientID",
         "PatientBirth", "PatientSex_DICOM", "Pediatric",
         "Labels", "group", "Partition", "Subject_occurrences",
         "Partition_occurrences", "Partitionlabel_occurrences"]
 
+# Read the data from the TSV file into a DataFrame
 reporty = pd.read_csv('../Data/neumo_dataset_balanced_0.tsv', usecols=cols, sep='\t')
 
+# Explode the 'Labels' column to separate multi-label entries
 df_exploded = reporty.explode('Labels')
 
-# Count the occurrences of each value
+# Count the occurrences of each label
 value_counts = df_exploded['Labels'].value_counts()
-
-print(value_counts[0:10])
-
 
 # ['normal']                            9004
 # ['pneumonia']                          783
@@ -22,6 +22,14 @@ print(value_counts[0:10])
 # ['consolidation', ' pneumonia']        210
 # ['consolidation']                      4
 
+# Print the top 10 most frequent labels
+print(value_counts[0:10])
+
+# Slice the DataFrame to include only rows with specific labels
 sliced = reporty[reporty['Labels'].isin(["['normal']", "['consolidation', ' pneumonia']"])]
+
+# Print the length of the sliced DataFrame
 print(len(sliced))
+
+# Write the sliced DataFrame to a CSV file
 sliced.to_csv("sliced.csv", encoding='utf-8', index=False)
