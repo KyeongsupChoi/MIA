c13535d0fc2ce246346ceacd27b9bb5dbfc9b0f1
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-03-28 13:40:49 +0900

    cookiecutter

---
+++ MIA/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analyzing CR images utilizing PyTorch and OpenCV, with insights from relevant research papers',
+    author='Kyeongsup Choi',
+    license='MIT',
+)
