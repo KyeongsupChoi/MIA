1e31cf68d73dac20e6d59907cfe73b2dfaa1165e
Kyeongsup Choi <kyeongsupchoi@gmail.com>
2024-06-03 09:27:55 +0900

    EDA

--- src/visualization/exploratory.py
+++ src/visualization/exploratory.py
@@ -1,14 +1,24 @@
 import pandas as pd
 
 # Define columns of interest
-cols = ["ImageID", "StudyDate_DICOM", "StudyID", "PatientID",
-        "PatientBirth", "PatientSex_DICOM", "Pediatric",
-        "Labels", "group", "Partition", "Subject_occurrences",
-        "Partition_occurrences", "Partitionlabel_occurrences"]
+cols = ["ImageID", "PatientID", "PatientBirth",
+        "Projection", "Pediatric", "Modality_DICOM",
+        "Manufacturer_DICOM", "Labels", "group"]
 
 # Read the data from the TSV file into a DataFrame
-reporty = pd.read_csv('../../data/raw/neumo_dataset_balanced_0.tsv', usecols=cols, sep='\t')
+reporty = pd.read_csv('../../data/raw/neumo_dataset.tsv', usecols=cols, sep='\t')
 
+print(reporty.describe())
+
+print(reporty.shape)
+
+print(reporty.columns)
+
+print(reporty.info())
+
+print(reporty.head().to_string())
+
+print(reporty['Projection'].value_counts())
 
 # Explode the 'Labels' column to separate multi-label entries
 df_exploded = reporty.explode('Labels')
@@ -16,21 +26,27 @@ df_exploded = reporty.explode('Labels')
 # Count the occurrences of each label
 value_counts = df_exploded['Labels'].value_counts()
 
-# ['normal']                            9004
-# ['pneumonia']                          783
-# ['infiltrates']                        739
-# ['infiltrates', ' pneumonia']          478
-# ['consolidation', ' pneumonia']        210
-# ['consolidation']                      4
-
 # Print the top 10 most frequent labels
-#print(value_counts[0:10])
+print(value_counts[0:10])
 
 # Slice the DataFrame to include only rows with specific labels
 sliced = reporty[reporty['Labels'].isin(["['normal']", "['consolidation', ' pneumonia']"])]
 
 # Print the length of the sliced DataFrame
-#print(len(sliced))
+print(sliced)
 
 # Write the sliced DataFrame to a CSV file
-sliced.to_csv("sliced.csv", encoding='utf-8', index=False)
+#sliced.to_csv("sliced.csv", encoding='utf-8', index=False)
+
+"""
+Labels
+['normal']                            9004
+['pneumonia']                          783
+['infiltrates']                        739
+['infiltrates', ' pneumonia']          478
+['consolidation', ' pneumonia']        210
+['COPD signs']                         156
+['alveolar pattern', ' pneumonia']     156
+['scoliosis']                          137
+['infiltrates', 'unchanged']           134
+['alveolar pattern', 'pneumonia']      125"""
